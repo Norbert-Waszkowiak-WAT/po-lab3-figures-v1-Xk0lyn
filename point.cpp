30273e52ba09a6a21f6df22c161ad3cae87a1f47
Xk0lyn <jandzwonkowski210@gmail.com>
2025-12-15 07:42:18 +0000

    wa

--- point.cpp
+++ point.cpp
@@ -1,42 +1,29 @@
 #include "point.h"
-
-Point::Point(double x, double y) : x(x), y(y) {}
-
-double Point::getX() const { return x; }
-double Point::getY() const { return y; }
-
-void Point::setX(double newX) { x = newX; }
-void Point::setY(double newY) { y = newY; }
-
-std::string Point::toString() const {
-    std::ostringstream oss;
-    oss << "Point(";
-    oss << std::fixed << std::setprecision(1);
-    oss << x << ", " << y << ")";
-    return oss.str();
+#include <sstream>
+#include <iomanip>
+ 
+Point::Point(double x, double y): x(x) , y(y){}
+Point::Point(const Point &other) : x(other.x), y(other.y){}
+bool Point::equals(Point &other){
+    return x == other.x && y == other.y;
 }
-
-bool Point::equals(const Point& other) const {
-    return (x == other.x) && (y == other.y);
+double const Point::getX(){
+    return x;
 }
-
-void Point::flip() {
+double const Point::getY() {
+    return y;
+}
+void Point::flip(){
     x = -x;
     y = -y;
 }
-
-void Point::move(double dx, double dy) {
-    x += dx;
-    y += dy;
+void Point::move(double x, double y){
+    this -> x+=x;
+    this -> y += y;
 }
-
-double Point::distance(const Point& other) const {
-    double dx = x - other.x;
-    double dy = y - other.y;
-    return std::sqrt(dx*dx + dy*dy);
-}
-
-std::ostream& operator<<(std::ostream& os, const Point& p) {
-    os << "(" << p.getX() << ", " << p.getY() << ")";
-    return os;
+std::string Point::toString(){
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(1);
+    oss << "Point(" << x << ", "<< y << ")";
+    return oss.str();
 }
--- point.h
+++ point.h
@@ -1,34 +1,19 @@
-#ifndef POINT_H
-#define POINT_H
-
+#ifndef POINT
+#define POINT
 #include <iostream>
-#include <string>
-#include <cmath>
-#include <sstream>
-#include <iomanip>
-
-class Point {
-private:
-    double x;
-    double y;
-
-public:
-    Point(double x = 0, double y = 0);
-    
-    double getX() const;
-    double getY() const;
-    
-    void setX(double newX);
-    void setY(double newY);
-    
-    std::string toString() const;
-    bool equals(const Point& other) const;
-    void flip();
-    void move(double dx, double dy);
-    
-    double distance(const Point& other) const;
-    
-    friend std::ostream& operator<<(std::ostream& os, const Point& p);
+ 
+class Point{
+    private:
+        double x;
+        double y;
+    public:
+        Point(double x, double y);
+        Point(const Point &other);
+        bool equals(Point &other);
+        double const getX();
+        double const getY();
+        void flip();
+        void move(double x, double y);
+        std::string toString();
 };
-
 #endif

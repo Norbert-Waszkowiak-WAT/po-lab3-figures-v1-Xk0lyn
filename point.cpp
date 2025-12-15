a86944696d6013a2c5cd1f9b08c3d106b4166cce
Xk0lyn <jandzwonkowski210@gmail.com>
2025-12-15 07:45:32 +0000

    hv

--- point.cpp
+++ point.cpp
@@ -1,29 +1,42 @@
 #include "point.h"
-#include <sstream>
-#include <iomanip>
- 
-Point::Point(double x, double y): x(x) , y(y){}
-Point::Point(const Point &other) : x(other.x), y(other.y){}
-bool Point::equals(Point &other){
-    return x == other.x && y == other.y;
+
+Point::Point(double x, double y) : x(x), y(y) {}
+
+double Point::getX() const { return x; }
+double Point::getY() const { return y; }
+
+void Point::setX(double newX) { x = newX; }
+void Point::setY(double newY) { y = newY; }
+
+std::string Point::toString() const {
+    std::ostringstream oss;
+    oss << "Point(";
+    oss << std::fixed << std::setprecision(1);
+    oss << x << ", " << y << ")";
+    return oss.str();
 }
-double const Point::getX(){
-    return x;
+
+bool Point::equals(const Point& other) const {
+    return (x == other.x) && (y == other.y);
 }
-double const Point::getY() {
-    return y;
-}
-void Point::flip(){
+
+void Point::flip() {
     x = -x;
     y = -y;
 }
-void Point::move(double x, double y){
-    this -> x+=x;
-    this -> y += y;
+
+void Point::move(double dx, double dy) {
+    x += dx;
+    y += dy;
 }
-std::string Point::toString(){
-    std::ostringstream oss;
-    oss << std::fixed << std::setprecision(1);
-    oss << "Point(" << x << ", "<< y << ")";
-    return oss.str();
+
+double Point::distance(const Point& other) const {
+    double dx = x - other.x;
+    double dy = y - other.y;
+    return std::sqrt(dx*dx + dy*dy);
+}
+
+std::ostream& operator<<(std::ostream& os, const Point& p) {
+    os << "(" << p.getX() << ", " << p.getY() << ")";
+    return os;
 }
